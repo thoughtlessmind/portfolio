216eb14d8ed75d1fe42a52d80c4b2e56a183702a
Rajiv <1998irajeev@gmail.com>
2020-04-09 14:10:09 +0530

    introduced fixed nav bar

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log('hello')
 const moveToAbout = () =>{
     document.getElementById('about').scrollIntoView(true)
 }
+
+const moveToWork = () =>{
+    document.getElementById('work').scrollIntoView()
+}
