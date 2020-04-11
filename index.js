f574581931baa6e91124ce928de8fe5fa9ea1fe3
Rajiv <1998irajeev@gmail.com>
2020-04-11 22:21:14 +0530

    introduced intersection observer

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('hello')
 
 const moveToAbout = () =>{
     document.getElementById('about').scrollIntoView(true)
@@ -7,3 +6,44 @@ const moveToAbout = () =>{
 const moveToWork = () =>{
     document.getElementById('work').scrollIntoView()
 }
+
+// window.addEventListener("scroll", (event)=>{
+//     console.log("scrolled")
+//     var scroll = this.scrollY
+//     if(scroll > 20){
+//         document.getElementById('about').scrollIntoView({
+//             block: 'start',
+//             behavior: 'smooth'
+//         })
+//     }
+// })
+
+const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-150px"
+}
+
+const header = document.querySelector("header")
+const sections = document.querySelectorAll(".section")
+const mainSection = document.querySelector(".main-container")
+
+const sectionOne = document.querySelector(".about-section");
+
+const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            header.classList.toggle("green-nav")
+        }
+        console.log(entry.target, '-', entry.isIntersecting)
+        // entry.target.classList.toggle('inverse')
+    });
+}, options)
+
+// observer.observe(sectionOne)
+
+// sections.forEach(section =>{
+//     observer.observe(section)
+// })
+
+observer.observe(mainSection)
