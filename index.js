9ca6668f76ae42c44af0b2a6e904feaeaa030d44
Rajiv <1998irajeev@gmail.com>
2020-04-12 18:43:29 +0530

    added snap scroling

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const moveToWork = () =>{
     document.getElementById('work').scrollIntoView()
 }
 
-// window.addEventListener("scroll", (event)=>{
-//     console.log("scrolled")
-//     var scroll = this.scrollY
-//     if(scroll > 20){
-//         document.getElementById('about').scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// })
 
 const options = {
     root: null,
@@ -32,19 +22,9 @@ const sectionOne = document.querySelector(".about-section");
 
 const observer = new IntersectionObserver(function(entries, observer){
     entries.forEach(entry =>{
-        // if(!entry.isIntersecting){
-        //     header.classList.toggle("green-nav")
-        // }
         entry.isIntersecting ? header.classList.remove("green-nav") : header.classList.add("green-nav")
         console.log(entry.target, '-', entry.isIntersecting)
-        // entry.target.classList.toggle('green-nav')
     });
 }, options)
-
-// observer.observe(sectionOne)
-
-// sections.forEach(section =>{
-//     observer.observe(section)
-// })
 
 observer.observe(mainSection)
